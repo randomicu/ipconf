40860e4f0c7a443c2cf1c345a9afa612b27ffe82
pythoninja <pythoninja@users.noreply.github.com>
2021-08-31 20:18:46 +0300

    test: add smoke tests
    Closes #6

--- src/test/java/cc/ipconf/ipconfcc/IpConfApplicationTests.java
+++ src/test/java/cc/ipconf/ipconfcc/IpConfApplicationTests.java
@@ -1,13 +1,35 @@
 package cc.ipconf.ipconfcc;
 
+import cc.ipconf.ipconfcc.controllers.AsnController;
+import cc.ipconf.ipconfcc.controllers.InfoController;
+import cc.ipconf.ipconfcc.controllers.IpController;
+import cc.ipconf.ipconfcc.controllers.LocationController;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IpConfApplicationTests {
 
+	@Autowired
+	private AsnController asnController;
+
+	@Autowired
+	private InfoController infoController;
+
+	@Autowired
+	private IpController ipController;
+
+	@Autowired
+	private LocationController locationController;
+
 	@Test
 	void contextLoads() {
+		assertThat(asnController).isNotNull();
+		assertThat(infoController).isNotNull();
+		assertThat(ipController).isNotNull();
+		assertThat(locationController).isNotNull();
 	}
 
 }
