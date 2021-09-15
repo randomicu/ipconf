642d81e21985d33fa5d786a649b84aafe2dba16f
pythoninja <pythoninja@users.noreply.github.com>
2021-09-15 21:40:08 +0300

    test: add tests to ASN controller

---
+++ src/test/java/cc/ipconf/AppPaths.java
@@ -0,0 +1,9 @@
+package cc.ipconf;
+
+public class AppPaths {
+
+  public static String API_PATH = "/api";
+  public static String API_VERSION = "v1";
+  public static String ASN_ENDPOINT = "%s/%s/asn".formatted(API_PATH, API_VERSION);
+
+}
