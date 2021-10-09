9c500336fdb2d07d17e39b06ee2c944b416461f6
pythoninja <pythoninja@users.noreply.github.com>
2021-10-09 22:44:26 +0300

    test: add tests to generateDatabasePath

---
+++ src/test/java/cc/ipconf/FilenameGeneratorTest.java
@@ -0,0 +1,33 @@
+package cc.ipconf;
+
+import cc.ipconf.config.MMDBLoaderConfig;
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class FilenameGeneratorTest {
+
+  MMDBLoaderConfig mmdbLoaderConfig;
+  String databasePath;
+
+  @BeforeEach
+  void setUp() {
+    mmdbLoaderConfig = new MMDBLoaderConfig();
+    ReflectionTestUtils.setField(mmdbLoaderConfig, "databasesDirectoryPath", "mmdb/");
+  }
+
+  @Test
+  void testDatabasePathGenerator() {
+    String databasePathPattern = "city-%%ACTUAL_DATE%%.mmdb";
+    LocalDate date = LocalDate.now();
+    var month = date.getMonthValue();
+    var year = date.getYear();
+
+    databasePath = ReflectionTestUtils.invokeMethod(mmdbLoaderConfig, "generateDatabasePath", databasePathPattern);
+
+    assertEquals("mmdb/city-%s-%s.mmdb".formatted(year, month), databasePath);
+  }
+
+}
