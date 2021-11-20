b4aeb1e06699e004aef45f0a5c5219bd21c62a77
pythoninja <pythoninja@users.noreply.github.com>
2021-11-20 18:38:28 +0300

    refactor: extract shared method to load geodb

--- src/main/java/cc/ipconf/config/GeoDatabaseLoaderConfig.java
+++ src/main/java/cc/ipconf/config/GeoDatabaseLoaderConfig.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,32 +35,31 @@ public class GeoDatabaseLoaderConfig {
   @Bean
   public DatabaseReader getCityDatabaseReader() {
     String cityDatabasePath = generateDatabasePath(cityDatabaseNamePattern, databasesDirectory);
-    File database = new File(Paths.get(cityDatabasePath).toString());
-    cityDatabaseName = database.getName();
-    DatabaseReader.Builder databaseReaderBuilder = new DatabaseReader.Builder(database);
+    File databaseFile = new File(Paths.get(cityDatabasePath).toString());
+    cityDatabaseName = databaseFile.getName();
 
-    try {
-      log.info("Loading City database: {}", database.getAbsolutePath());
-      return databaseReaderBuilder.build();
-    } catch (IOException e) {
-      Sentry.captureException(e);
-      throw new IllegalArgumentException("City database file not found");
-    }
+    return getDatabaseReader(databaseFile);
   }
 
   @Bean
   public DatabaseReader getAsnDatabaseReader() {
     String asnDatabasePath = generateDatabasePath(asnDatabaseNamePattern, databasesDirectory);
-    File database = new File(Paths.get(asnDatabasePath).toString());
-    asnDatabaseName = database.getName();
+    File databaseFile = new File(Paths.get(asnDatabasePath).toString());
+    asnDatabaseName = databaseFile.getName();
+
+    return getDatabaseReader(databaseFile);
+  }
+
+  @NotNull
+  private DatabaseReader getDatabaseReader(File database) {
     DatabaseReader.Builder databaseReaderBuilder = new DatabaseReader.Builder(database);
 
     try {
-      log.info("Loading ASN database: {}", database.getAbsolutePath());
+      log.info("Loading database: {}", database.getAbsolutePath());
       return databaseReaderBuilder.build();
     } catch (IOException e) {
       Sentry.captureException(e);
-      throw new IllegalArgumentException("ASN database file not found");
+      throw new IllegalArgumentException("Geo database file not found");
     }
   }
 
